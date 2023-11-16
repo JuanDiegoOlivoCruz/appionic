bf309adac41b25285f6cc05ee9a5c559950c24c1
JuanDiegoOlivoCruz <juandiego.olivoc@gmail.com>
2023-11-16 15:34:56 -0600

    Primercommitt

---
+++ src/app/core/trello/entities/cards.ts
@@ -0,0 +1,9 @@
+
+export class Cards {
+    "id":string;
+    "idList":string;
+    "key":string = "c0dbbf9f26a6731f88688f40baf4474e";
+    "token":string = "ATTA6d8d14693b1f24929fc488646f3b97ae36dc6c8264365d124b5f0130805f31595CE76C5F";
+    "name": string;
+    "desc": string
+}
